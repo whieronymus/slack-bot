4e4134f63d06bd5d973a4e82886955efe2fdb317
Raaaven <kristian.smith.2001@hotmail.co.uk>
2017-12-02 23:58:18 +0000

    changed ctx to context

--- ravenbot/bot.py
+++ ravenbot/bot.py
@@ -16,21 +16,21 @@ class Bot(SlackBot):
     @SlackCommand()
     def ping(context):
         """Pong!"""
-        ctx.send(":table_tennis_paddle_and_ball: Pong!")
+        context.send(":table_tennis_paddle_and_ball: Pong!")
 
     @SlackCommand()
     def help(context, command=None):
         """Get a list of commands or extra information"""
         if command in SlackCommand.commands:  # If command specified exists
-            cmd = ctx.bot.get_command(command)  # Get the command
+            cmd = context.bot.get_command(command)  # Get the command
             help_info = "*Description:* " + cmd.get("callback").__doc__  # Description is docstring
-            help_info += "\n*Usage:* `" + ctx.bot.get_usage(cmd) + "`"  # Get usage
+            help_info += "\n*Usage:* `" + context.bot.get_usage(cmd) + "`"  # Get usage
         else:
             help_info = "*--------------------- Help Commands ---------------------*"
             for cmd in SlackCommand.commands:
                 help_info += "\n• `" + cmd + "`"  # List commands
             help_info += "\n\n Type `help <command>` for more info on that command"
-        ctx.send(help_info)
+        context.send(help_info)
 
     @SlackCommand()
     def joke(context):
@@ -40,7 +40,7 @@ class Bot(SlackBot):
                                                                         "From": "https://github.com/whieronymus"})
         json_load = json.loads(response.text)
         if json_load.get("status") == 200:
-            ctx.send(json_load.get("joke"))
+            context.send(json_load.get("joke"))
 
     def on_member_join_team(self, **output):
         user = output.get("user").get("profile")
