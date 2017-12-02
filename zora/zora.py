dbe6f4869337ba6d4557cb9df96cf52f2d2593e2
whieronymus <whieronymus@gmail.com>
2017-12-02 15:11:37 -0500

    added import command

--- zora/zora.py
+++ zora/zora.py
@@ -25,6 +25,44 @@ def ditto_cmd(cmd):
     """
     return " ".join(cmd.split()[1:])
 
+def import_cmd(cmd):
+    """
+    Returns valuable reminders, code, tutorials, etc
+    """
+    module = cmd.split()[1:]
+    default = "No module named {}".format(module)
+
+    zen = """
+```The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!```
+"""
+
+    if module = "this":
+        response = zen
+    else:
+        response = default
+
+    return response
+
 
 def cats_cmd(cmd):
     eats = random.randint(1, 5)
@@ -158,12 +196,15 @@ def define_commands():
     Command(help_cmd,
             "help",
             "Displays help for given command or all if none given.")
-    Command(cats_cmd, 
+    Command(cats_cmd,
             "yummy",
             "Returns a picture of a cat")
-    Command(greet_cmd, 
+    Command(greet_cmd,
             "hi",
-            "Says hi in a random in a random language")  
+            "Says hi in a random in a random language")
+    Command(remind_raven,
+            "import",
+
 
 
 def main():
